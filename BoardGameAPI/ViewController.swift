824f6e36145de8b8129e588bf34bf4868c549654
Shien <f0928652413@gmail.com>
2022-06-02 17:13:12 +0800

    Initial Commit

---
+++ BoardGameAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoardGameAPI
+//
+//  Created by Shien on 2022/6/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
